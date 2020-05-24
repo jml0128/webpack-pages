fb46280d2e1b71869e44a3c1dda66f49f3db57c8
jml0128 <361029007@qq.com>
2020-05-24 10:52:03 +0800

    ankang web page

--- src/config/pages.js
+++ src/config/pages.js
@@ -1,20 +1,42 @@
-/*
- * @Author: your name
- * @Date: 2020-04-14 11:32:50
- * @LastEditTime: 2020-05-24 10:20:34
- * @LastEditors: jml
- * @Description: In User Settings Edit
- * @FilePath: \06-zw-page\src\config\pages.js
- */
 module.exports = {
   index: {
-    title: '专业音乐舞蹈艺术培训',
-    html: '/pages/art/index.ejs',
-    js: '/pages/art/index.js',
+    title: '首页',
+    html: '/pages/index/index.ejs',
+    js: '/pages/index/index.js',
   },
-  activity: {
-    title: '专业音乐舞蹈艺术培训',
-    html: '/pages/artmobile/index.ejs',
-    js: '/pages/artmobile/index.js',
+  about: {
+    title: '关于我们',
+    html: '/pages/about/about.ejs',
+    js: '/pages/about/about.js',
+  },
+  news: {
+    title: '企业动态',
+    html: '/pages/news/index.ejs',
+    js: '/pages/news/index.js',
+  },
+  service: {
+    title: '业务范围',
+    html: '/pages/service/index.ejs',
+    js: '/pages/service/index.js',
+  },
+  company: {
+    title: '合作单位',
+    html: '/pages/company/index.ejs',
+    js: '/pages/company/index.js',
+  },
+  example: {
+    title: '项目案例',
+    html: '/pages/example/index.ejs',
+    js: '/pages/example/index.js',
+  },
+  team: {
+    title: '团队介绍',
+    html: '/pages/team/index.ejs',
+    js: '/pages/team/index.js',
+  },
+  article: {
+    title: '文章详情',
+    html: '/pages/article/index.ejs',
+    js: '/pages/article/index.js',
   },
 }
