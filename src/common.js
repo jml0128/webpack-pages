bed4c66627b7143a33ecbb6d87c95e341d1c36c5
jml0128 <361029007@qq.com>
2020-05-24 10:58:26 +0800

    init xaxdyk-art-web

--- src/common.js
+++ src/common.js
@@ -1,19 +1,2 @@
 import '@/assets/scss/reset.scss'
 import '@/assets/scss/common.scss'
-
-window.showNav = (e) => {
-  const navDom = document.getElementById('nav')
-  const status = e.getAttribute('data-v')
-  const n = require('@/assets/img/n.png').default
-  const c = require('@/assets/img/c.png').default
-  if (status === 'n') {
-    e.src = c
-    e.setAttribute('data-v', 'v')
-    navDom.style.display = 'flex'
-  } else {
-    e.src = n
-    e.setAttribute('data-v', 'n')
-    navDom.style.display = 'none'
-  }
-  console.log(status)
-}
