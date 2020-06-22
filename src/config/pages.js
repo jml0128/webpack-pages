a7a5a4b13011432bfe2920cdc95c48575ee3c258
jml0128 <361029007@qq.com>
2020-06-23 02:55:14 +0800

    完成新闻列表

--- src/config/pages.js
+++ src/config/pages.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-04-14 11:32:50
- * @LastEditTime: 2020-05-24 10:36:14
+ * @LastEditTime: 2020-06-23 02:31:42
  * @LastEditors: jml
  * @Description: In User Settings Edit
  * @FilePath: \06-zw-page\src\config\pages.js
@@ -11,5 +11,15 @@ module.exports = {
     title: 'page-test',
     html: '/pages/index/index.ejs',
     js: '/pages/index/index.js',
+  },
+  about: {
+    title: 'page-test',
+    html: '/pages/about/index.ejs',
+    js: '/pages/about/index.js',
+  },
+  news: {
+    title: 'page-test',
+    html: '/pages/news/index.ejs',
+    js: '/pages/news/index.js',
   },
 }
